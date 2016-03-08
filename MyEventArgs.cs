0fd7cc03990ac19784ace8798d522cc401e00b23
webshark9 <mjthiessen-cc@conestogac.on.ca>
2016-03-08 14:38:10 -0500

    Added MyEventArgs class
    used to send event arguments to a method

---
+++ MyEventArgs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+
+namespace Atlas
+{
+    class MyEventArgs : EventArgs
+    {
+        private TcpClient sock;
+        public TcpClient clientSock
+        {
+            get { return sock; }
+            set { sock = value; }
+        }
+
+        public MyEventArgs(TcpClient tcpClient)
+        {
+            sock = tcpClient;
+        }
+
+
+    }
+}
